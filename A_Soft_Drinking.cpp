f50186ddde04a9fa68554329056498869c2283b6
Omkar-B-More <omkarmore8103@gmail.com>
2025-12-14 20:00:25 +0530

    Added A_Soft_Drinking.cpp from practice history

---
+++ A_Soft_Drinking.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k,l,c,d,p,nl,np,x,y,z;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    x=(k*l)/nl;
+    y=c*d;
+    z=p/np;
+    cout<<min(x,min(y,z))/n;
+
+}
