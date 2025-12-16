ae0810ea30f93cf7b74530f1ab58bf1ef9ef49bf
Omkar-B-More <omkarmore8103@gmail.com>
2025-12-16 20:42:07 +0530

    Added A_Restoring_Three_Numbers.cpp from practice history

---
+++ A_Restoring_Three_Numbers.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,f=0;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a,a+n);
+        for(int i=1;i<n;i++){
+            if(a[i]-a[i-1]>1){
+                cout<<"NO"<<endl;
+                f=1;
+                break;
+            }
+        }
+        if(!f){
+            cout<<"YES"<<endl;
+        }
+            
+    }
+        
+}  
