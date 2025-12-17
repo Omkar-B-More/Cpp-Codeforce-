38d01d0259d7662461e346021d5223779c90db97
Omkar-B-More <omkarmore8103@gmail.com>
2025-12-17 21:23:32 +0530

    Added A_Sereja_and_Dima.cpp from practice history

---
+++ A_Sereja_and_Dima.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n],b[2]={0,0},l=0,r=n-1;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(a[l]>a[r]){
+            b[i%2]+=a[l];
+            l++;
+        }
+        else{
+            b[i%2]+=a[r];
+            r--;
+        }
+    }
+    cout<<b[0]<<" "<<b[1];
+}
+
