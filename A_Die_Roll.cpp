3cf38b8d86fa254a4bd537fc3c601a2fae62526b
Omkar-B-More <omkarmore8103@gmail.com>
2025-12-22 15:02:19 +0530

    Added A_Die_Roll.cpp from practice history

---
+++ A_Die_Roll.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int a,b,c=0,d;
+    cin>>a>>b;
+    d=6-max(a,b)+1;
+;    cout<<d/__gcd(d,6)<<"/"<<6/__gcd(d,6);
+    
+}
+
+int main() {
+    fast_io;
+   solve();
+    return 0;
+}
