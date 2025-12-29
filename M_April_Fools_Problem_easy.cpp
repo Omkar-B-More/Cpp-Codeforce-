7d1d6ec5555b9fe19e4411fdc208f7263a782750
Omkar-B-More <omkarmore8103@gmail.com>
2025-12-29 21:00:49 +0530

    Added M_April_Fools_Problem_easy.cpp from practice history

---
+++ M_April_Fools_Problem_easy.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n,k,c=0;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<k;i++){
+        c+=a[i];
+    }
+    cout<<c;
+}
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
