6e41a6769d9c5172c5966ed3ab6cee3e391f4290
Omkar-B-More <omkarmore8103@gmail.com>
2025-12-30 20:53:22 +0530

    Added A_Boring_Apartments.cpp from practice history

---
+++ A_Boring_Apartments.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    string s;
+    cin>>s;
+    cout<<(s[0]-'1')*10+s.size()*(s.size()+1)/2<<"\n";
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
