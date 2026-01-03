da999809674f1c354fc263416d39aeb5206de24b
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-03 21:16:26 +0530

    Added B_Honest_Coach.cpp from practice history

---
+++ B_Honest_Coach.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    int a[n],x=INT_MAX;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=1;i<n;i++){
+        x=min(x,(a[i]-a[i-1]));
+    }
+    cout<<x<<"\n";
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
