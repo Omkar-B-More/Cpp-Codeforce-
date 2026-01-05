c73f52985ff40f08b084cd24842c9e12a649f875
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-05 15:08:57 +0530

    Added A_Magical_Sticks.cpp from practice history

---
+++ A_Magical_Sticks.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n/2<<"\n";
+    }
+    else{
+        cout<<n/2+1<<"\n";
+    }
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
