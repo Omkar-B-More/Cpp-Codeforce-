80c25378c583780f2a93f019891b3a90b5e5ed75
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-06 20:35:53 +0530

    Added A_Minimize.cpp from practice history

---
+++ A_Minimize.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int a,b;
+    cin>>a>>b;
+    cout<<b-a<<"\n";
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
