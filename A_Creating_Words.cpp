7bff293f89463b0ac739e14524808fb9fb92ee87
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-08 20:10:27 +0530

    Added A_Creating_Words.cpp from practice history

---
+++ A_Creating_Words.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    string a,b;
+    cin>>a>>b;
+    swap(a[0],b[0]);
+    cout<<a<<" "<<b<<"\n";
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
