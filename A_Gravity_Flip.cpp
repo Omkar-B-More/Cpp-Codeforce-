a30555e07a2d91aef106aa5a55121065c24e5d82
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-15 15:30:40 +0530

    Added A_Gravity_Flip.cpp from practice history

---
+++ A_Gravity_Flip.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i:a){
+        cout<<i<<" ";
+    }
+}
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
