4f61731f7999773352398f115389b370c21927fc
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-18 20:29:00 +0530

    Added B_Prefix_Max.cpp from practice history

---
+++ B_Prefix_Max.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n,ans=0;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    ans=a[n-1]*n;    
+    cout<<ans<<"\n";
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
