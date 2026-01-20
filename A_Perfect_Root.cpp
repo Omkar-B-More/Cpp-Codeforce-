fded07d157eb107eec08386a7f9f4fe67793cbc3
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-20 13:14:26 +0530

    Added A_Perfect_Root.cpp from practice history

---
+++ A_Perfect_Root.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<i+1<<" ";
+    }
+    cout<<"\n";
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
