43cca4a7074daeba4b4597dd50976e1e5504f4f3
Omkar-B-More <omkarmore8103@gmail.com>
2026-01-21 13:49:39 +0530

    Added Protein_Diet.cpp from practice history

---
+++ Protein_Diet.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int a,b;
+    cin>>a>>b;
+    if(a>=b){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+}
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
