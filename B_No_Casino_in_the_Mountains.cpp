bc507b73e712ffecaf729c92400aa5e55268e529
Omkar-B-More <omkarmore8103@gmail.com>
2026-02-13 20:32:13 +0530

    Added B_No_Casino_in_the_Mountains.cpp from practice history

---
+++ B_No_Casino_in_the_Mountains.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n=4,k=2,c=0;
+    cin>>n>>k;
+    int a[4];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    // a[0]=0,a[1]=1,a[2]=0,a[3]=1;
+    for(int i=0;i<n;i++){
+        if(a[i]==0){
+            c++;
+            i+=k;
+        }
+    }
+    cout<<c<<"\n";
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
