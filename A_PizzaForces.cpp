10c4afc825b736037a1a94c555d1f49521349930
Omkar-B-More <omkarmore8103@gmail.com>
2026-03-02 20:21:09 +0530

    Added A_PizzaForces.cpp from practice history

---
+++ A_PizzaForces.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    long long n;
+    cin>>n;
+    cout<<max(6LL,n+1)/2*5<<endl;
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
