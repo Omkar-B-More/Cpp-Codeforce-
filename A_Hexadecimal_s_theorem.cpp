9a2fb80bf0f3cb00e8a803fe071ec5c3d3535f83
Omkar-B-More <omkarmore8103@gmail.com>
2026-03-24 13:40:33 +0530

    Added A_Hexadecimal_s_theorem.cpp from practice history

---
+++ A_Hexadecimal_s_theorem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    cout<<n<<" "<<0<<" "<<0;
+}
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
