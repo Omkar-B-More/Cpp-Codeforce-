fd6c8b6f867e576e402aceef402af6fc54471bfc
Omkar-B-More <omkarmore8103@gmail.com>
2026-04-02 14:28:14 +0530

    Added A_Twice.cpp from practice history

---
+++ A_Twice.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>a;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    if(n==1){
+        cout<<0;
+        return;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+          cin>>  
+        }
+    }
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
