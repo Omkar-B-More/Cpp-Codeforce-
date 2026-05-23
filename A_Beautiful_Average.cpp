fd20db6cc44501db46d5d5a23b96787183aa13fc
Omkar-B-More <omkarmore8103@gmail.com>
2026-05-23 13:02:02 +0530

    Added A_Beautiful_Average.cpp from practice history

---
+++ A_Beautiful_Average.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    int s[n];
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+    sort(s,s+n);
+    cout<<s[n-1]<<endl;
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
