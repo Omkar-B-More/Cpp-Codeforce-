bee8c6997392277ea93053ae5460cd11076135db
Omkar-B-More <omkarmore8103@gmail.com>
2026-06-08 20:24:43 +0530

    Added B_Upscaling.cpp from practice history

---
+++ B_Upscaling.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    for(int i=0;i<2*n;i++){
+        int h=1;
+        for(int j=0;j<2*n;j++){
+        if(h==1||h==2){
+            h++;
+            cout<<"#";
+        }
+        else if(h==3||h==4){
+            h++;
+            cout<<".";
+        }
+        h==1;
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
