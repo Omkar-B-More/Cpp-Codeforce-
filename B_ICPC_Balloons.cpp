501bcb3593d673d5cd5c3abb557b78705efa20d0
Omkar-B-More <omkarmore8103@gmail.com>
2026-06-17 12:54:27 +0530

    Added B_ICPC_Balloons.cpp from practice history

---
+++ B_ICPC_Balloons.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    string s;
+    int n;
+    cin>>n;
+    int c=0;
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+    for(int i=0;i<n;i++){
+        
+    }
+
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
