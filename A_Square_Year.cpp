41ab21dc32f9e56e01acd064bca172a3f59f67ec
Omkar-B-More <omkarmore8103@gmail.com>
2026-07-21 22:25:06 +0530

    done

---
+++ A_Square_Year.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    int sq=ceil(sqrt(n));
+    if(sq*sq==n){
+        cout<<0<<" "<<sq<<endl;
+    }
+    else{
+        cout<<-1<<endl;
+    }
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
