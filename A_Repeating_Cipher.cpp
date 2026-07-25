28357d1ef58d3ef409e2ac5e8f20040034d4ff4c
Omkar-B-More <omkarmore8103@gmail.com>
2026-07-25 21:44:08 +0530

    done

---
+++ A_Repeating_Cipher.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    // vector<string>ans;
+    int i=0;
+    int add=1;
+    while(i<n){
+        cout<<s[i];
+        i+=add;
+        add++;
+    }
+
+}
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
